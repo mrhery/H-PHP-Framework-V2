bbdda1261acf361e450a24eb24ed3d38922e9041
mrhery <aimanmrc@gmail.com>
2018-08-19 12:59:37 +0800

    Edit cofig detail
    config detail has been changed

--- Systems/config.php
+++ Systems/config.php
@@ -3,9 +3,9 @@ require_once(__DIR__ . "/Misc/document_access.php");
 
 class Config{
 	public static $host 	= "127.0.0.1";
-	public static $database	= "userclou_user";
-	public static $username	= "userclou_user";
-	public static $password	= "user@1234567890";
+	public static $database	= "db_name";
+	public static $username	= "db_user";
+	public static $password	= "db_password";
 }
 
 ?>
