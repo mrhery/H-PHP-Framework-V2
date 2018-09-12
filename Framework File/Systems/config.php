1b731ec20d0faa960452335df2f457d4b2d04387
mrhery <aimanmrc@gmail.com>
2018-09-12 20:41:32 +0800

    config

--- Framework File/Systems/config.php
+++ Framework File/Systems/config.php
@@ -11,9 +11,9 @@ if(!isset($_SESSION["test"])){
 #Has been tested and developed on MySQL
 class Config{
 	public static $host 	= "127.0.0.1";
-	public static $database	= "travel_travel";
-	public static $username	= "travel_hery";
-	public static $password	= "hery@1234567890";
+	public static $database	= "";
+	public static $username	= "";
+	public static $password	= "";
 }
 
 #Put 1 to enable show error or otherwise put 0.
@@ -22,7 +22,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 #Put your website URL
-define("PORTAL", "https://workspace.intelhost.com.my/workspace/Intelhost_Travel_V2/");
+define("PORTAL", "https://website.com/");
 
 
 
